5166d308c2b58636a92b7858d55ac3fe0d9d69fd
MitraKumar <kapil@capitalnumbers.com>
2023-02-28 16:38:57 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo contains 2 porjects.
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Homepage</title>
+</head>
+<body>
+<ul>
+    <li><a href="../advance_php/assignment_1/index.php">Assignment 1</a></li>
+    <li><a href="../advance_php/assignment_2/index.php">Assignment 2</a></li>
+
+</ul>
+</body>
+</html>
