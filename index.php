314d871b57683bb0862d379325848672cb68395c
MitraKumar <kapil@capitalnumbers.com>
2023-03-01 10:58:31 +0530

    Path Update

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Homepage</title>
 </head>
-<body>
-<ul>
-    <li><a href="../advance_php/assignment_1/index.php">Assignment 1</a></li>
-    <li><a href="../advance_php/assignment_2/index.php">Assignment 2</a></li>
 
-</ul>
+<body>
+    <ul>
+        <li><a href="../FT2023-74/assignment_1/index.php">Assignment 1</a></li>
+        <li><a href="../FT2023-74/assignment_2/index.php">Assignment 2</a></li>
+    </ul>
 </body>
+
 </html>
