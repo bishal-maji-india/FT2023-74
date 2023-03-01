19d0cbd1a14d96491a8f6f5159f84e24c2988ca9
MitraKumar <kapil@capitalnumbers.com>
2023-03-01 10:50:05 +0530

    FT2023-74: Update-guzzle,indentation,comments.
    1.Use of guzzle insted of curl.
    2.Used proper indentation and comments.

--- assignment_1/utils.php
+++ assignment_1/utils.php
@@ -1,23 +1,14 @@
 <?php
+require './vendor/autoload.php';
+use GuzzleHttp\Client;
 class Helper{
     public $base_url='https://ir-dev-d9.innoraft-sites.com/';
 
+    //function to make api request using guzzle.
     function makeGetRequest($url){
-        $curlObj = curl_init();
-        curl_setopt_array($curlObj, array(
-          CURLOPT_URL => $url ,
-          CURLOPT_HTTPHEADER => array(
-            "Content-Type: text/plain",
-          ),
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => "",
-          CURLOPT_FOLLOWLOCATION => true,
-        ));
-
-        $response = curl_exec($curlObj);
-        curl_close($curlObj);
-        $decoded_result = json_decode($response, true);
+      $client =new Client();
+      $response = $client->request('GET', $url);
+        $decoded_result = json_decode($response->getBody(), true);
         return $decoded_result['data'];
     }
 }
-?>
